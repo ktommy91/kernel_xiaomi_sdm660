94c97293ce8d5d87c35c39a7f4697cd9a0f4dccb
ktommy91 <kemenesi.tamas@gmail.com>
2018-11-22 23:36:08 +0100

    disable tracing/logging part 6
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -825,7 +825,7 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 
 		ts->last_tick = hrtimer_get_expires(&ts->sched_timer);
 		ts->tick_stopped = 1;
-		trace_tick_stop(1, TICK_DEP_MASK_NONE);
+//		trace_tick_stop(1, TICK_DEP_MASK_NONE);
 	}
 
 	/*
