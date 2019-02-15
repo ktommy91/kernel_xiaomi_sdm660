052c7a21342d4ab49147d2cfb394727a40bbd1d6
Patrick Bellasi <patrick.bellasi@arm.com>
2019-02-15 11:49:36 +0000

    ANDROID: sched/fair: Don't double account RT util in boosted_cpu_util()
    WALT is a cross-class utilization tracking signal which accounts not
    only for FAIR tasks utilization but also for that of RT and DL tasks.
    PELT instead provides per-class utilization signals, which is why in
    boosted_cpu_util() we currently aggregated the FAIR utilization with
    that generated by RT tasks.
    
    However, the aggregation provided in boosted_cpu_util() is not correct
    when WALT is in use, since we end up double accounting RT tasks
    generated utilization. Even worst, we add a PELT signal to a WALT
    signal.
    
    Since:
       commit 63d1657d00e0 ("ANDROID: sched/rt: fix the problem that rt_rq's util is always zero.")
    the rt rq signal is correctly updated. However, that fix missed to
    detect and fix the unconditional aggregation of the PELT signal when
    WALT is in use.
    
    Fix this by ensuring to aggregate RT and FAIR utilization only when PELT
    is in use. Do that by refactoring boosted_cpu_util() to get just one
    "properly aggregated" signal and by making sure that the correct
    aggregation is preformed in cpu_util_freq(), which already provides
    WALT-PELT switching logic.
    
    Change-Id: Ifd738102e9102b210b7be2c2565ab796e3b57061
    Fixes: 63d1657d00e0 ANDROID: sched/rt: fix the problem that rt_rq's util is always zero.
    Reported-by: Ke Wang <ke.wang@spreadtrum.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3324,12 +3324,6 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 	return decayed;
 }
 
-unsigned long sched_get_rt_rq_util(int cpu)
-{
-	struct rt_rq *rt_rq = &(cpu_rq(cpu)->rt);
-	return rt_rq->avg.util_avg;
-}
-
 /**
  * attach_entity_load_avg - attach this entity to its cfs_rq load avg
  * @cfs_rq: cfs_rq to attach to
@@ -5053,9 +5047,9 @@ static inline unsigned long cpu_util(int cpu);
 
 static bool __cpu_overutilized(int cpu, int delta);
 static bool cpu_overutilized(int cpu);
-unsigned long boosted_cpu_util(int cpu, unsigned long other_util);
+unsigned long boosted_cpu_util(int cpu);
 #else
-#define boosted_cpu_util(cpu, other_util) cpu_util_freq(cpu)
+#define boosted_cpu_util(cpu) cpu_util_freq(cpu)
 #endif
 
 static unsigned long cpu_util_without(int cpu, struct task_struct *p);
@@ -6390,9 +6384,9 @@ schedtune_task_margin(struct task_struct *task)
 #endif /* CONFIG_SCHED_TUNE */
 
 unsigned long
-boosted_cpu_util(int cpu, unsigned long other_util)
+boosted_cpu_util(int cpu)
 {
-	unsigned long util = cpu_util_freq(cpu) + other_util;
+	unsigned long util = cpu_util_freq(cpu);
 	long margin = schedtune_cpu_margin(util, cpu);
 
 //	trace_sched_boost_cpu(cpu, util, margin);
@@ -7117,13 +7111,22 @@ static unsigned long cpu_util_without(int cpu, struct task_struct *p)
 	return min_t(unsigned long, util, capacity_orig_of(cpu));
 }
 
+static inline unsigned long cpu_util_rt(int cpu)
+{
+	struct rt_rq *rt_rq = &(cpu_rq(cpu)->rt);
+
+	return rt_rq->avg.util_avg;
+}
+
 static inline unsigned long cpu_util_freq(int cpu)
 {
 #ifdef CONFIG_SCHED_WALT
 	u64 walt_cpu_util;
 
-	if (unlikely(walt_disabled || !sysctl_sched_use_walt_cpu_util))
-		return cpu_util(cpu);
+	if (unlikely(walt_disabled || !sysctl_sched_use_walt_cpu_util)) {
+		return min(cpu_util(cpu) + cpu_util_rt(cpu),
+			   capacity_orig_of(cpu));
+	}
 
 	walt_cpu_util = cpu_rq(cpu)->prev_runnable_sum;
 	walt_cpu_util <<= SCHED_CAPACITY_SHIFT;
@@ -7131,7 +7134,7 @@ static inline unsigned long cpu_util_freq(int cpu)
 
 	return min_t(unsigned long, walt_cpu_util, capacity_orig_of(cpu));
 #else
-	return cpu_util(cpu);
+	return min(cpu_util(cpu) + cpu_util_rt(cpu), capacity_orig_of(cpu));
 #endif
 }
 
